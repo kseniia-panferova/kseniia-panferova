a2161f8477bc19147cc10e3d0abe8ad3d1e9b86a
kseniia-panferova <panferova.ksenia@gmail.com>
2026-05-06 18:14:02 +0200

    Fix project titles by removing underscores

--- projects.js
+++ projects.js
@@ -2,42 +2,42 @@ const projects = [
   {
     year: 2017,
     type: "S",
-    title: "2017_S_Navi_Pavilion",
+    title: "2017_S Navi Pavilion",
     slug: "2017_S_Navi_Pavilion",
     cover: "assets/img/2017_S_Navi_Pavilion-cover.webp"
   },
   {
     year: 2021,
     type: "A",
-    title: "2021_A_Rybinskoye Reservoir",
+    title: "2021_A Rybinskoye Reservoir",
     slug: "2021_A_Rybinskoye Reservoir",
     cover: "assets/img/2021_A_Rybinskoye Reservoir-cover.webp"
   },
   {
     year: 2023,
     type: "C",
-    title: "2023_С_Last_Glow",
+    title: "2023_С Last Glow",
     slug: "2023_С_Last_Glow",
     cover: "assets/img/2023_С_Last_Glow-cover.webp"
   },
    {
     year: 2024,
     type: "S",
-    title: "2024_S_Factory_Reset",
+    title: "2024_S Factory Reset",
     slug: "2024_S_Factory_Reset",
     cover: "assets/img/2024_S_Factory_Reset-cover.webp"
   },
      {
     year: 2021,
     type: "W",
-    title: "2021_W_Pavlovskaya_27",
+    title: "2021_W Pavlovskaya 27",
     slug: "2021_W_Pavlovskaya_27",
     cover: "assets/img/2021_W_Pavlovskaya_27-cover.webp"
   },
        {
     year: 2020,
     type: "W",
-    title: "2020_W_Varshavskoye_Highway_141",
+    title: "2020_W Varshavskoye Highway 141",
     slug: "2020_W_Varshavskoye_Highway_141",
     cover: "assets/img/2020_W_Varshavskoye_Highway_141-cover.webp"
   },
