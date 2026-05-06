9c321bfb8bd364c9d19db4a43b8d938f8ccfac1c
kseniia-panferova <panferova.ksenia@gmail.com>
2026-05-06 18:24:55 +0200

    Update slugs to use spaces instead of underscores

--- projects.js
+++ projects.js
@@ -3,42 +3,42 @@ const projects = [
     year: 2017,
     type: "S",
     title: "2017_S Navi Pavilion",
-    slug: "2017_S_Navi_Pavilion",
+    slug: "2017_S Navi Pavilion",
     cover: "assets/img/2017_S_Navi_Pavilion-cover.webp"
   },
   {
     year: 2021,
     type: "A",
     title: "2021_A Rybinskoye Reservoir",
-    slug: "2021_A_Rybinskoye Reservoir",
+    slug: "2021_A Rybinskoye Reservoir",
     cover: "assets/img/2021_A_Rybinskoye Reservoir-cover.webp"
   },
   {
     year: 2023,
     type: "C",
     title: "2023_С Last Glow",
-    slug: "2023_С_Last_Glow",
+    slug: "2023_С Last Glow",
     cover: "assets/img/2023_С_Last_Glow-cover.webp"
   },
    {
     year: 2024,
     type: "S",
     title: "2024_S Factory Reset",
-    slug: "2024_S_Factory_Reset",
+    slug: "2024_S Factory Reset",
     cover: "assets/img/2024_S_Factory_Reset-cover.webp"
   },
      {
     year: 2021,
     type: "W",
     title: "2021_W Pavlovskaya 27",
-    slug: "2021_W_Pavlovskaya_27",
+    slug: "2021_W Pavlovskaya 27",
     cover: "assets/img/2021_W_Pavlovskaya_27-cover.webp"
   },
        {
     year: 2020,
     type: "W",
     title: "2020_W Varshavskoye Highway 141",
-    slug: "2020_W_Varshavskoye_Highway_141",
+    slug: "2020_W Varshavskoye Highway_141",
     cover: "assets/img/2020_W_Varshavskoye_Highway_141-cover.webp"
   },
 ];
