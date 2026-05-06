4172450d3d047faffbfed7cf3f20913acf08eb22
kseniia-panferova <panferova.ksenia@gmail.com>
2026-05-06 21:16:16 +0200

    fix titles

--- projects.js
+++ projects.js
@@ -2,49 +2,49 @@ const projects = [
   {
     year: 2017,
     type: "S",
-    title: "2017_S Navi Pavilion",
+    title: "Navi Pavilion",
     slug: "2017_S_Navi_Pavilion",
     cover: "assets/img/2017_S_Navi_Pavilion-cover.webp"
   },
   {
     year: 2021,
     type: "A",
-    title: "2021_A Rybinskoye Reservoir",
+    title: "Rybinskoye Reservoir",
     slug: "2021_A_Rybinskoye_Reservoir",
     cover: "assets/img/2021_A_Rybinskoye Reservoir-cover.webp"
   },
   {
     year: 2023,
     type: "C",
-    title: "2023_С Last Glow",
+    title: "Last Glow",
     slug: "2023_С_Last_Glow",
     cover: "assets/img/2023_С_Last_Glow-cover.webp"
   },
    {
     year: 2024,
     type: "S",
-    title: "2024_S Factory Reset",
+    title: "Factory Reset",
     slug: "2024_S_Factory_Reset",
     cover: "assets/img/2024_S_Factory_Reset-cover.webp"
   },
      {
     year: 2021,
     type: "W",
-    title: "2021_W Pavlovskaya 27",
+    title: "Pavlovskaya 27",
     slug: "2021_W_Pavlovskaya_27",
     cover: "assets/img/2021_W_Pavlovskaya_27-cover.webp"
   },
      {
     year: 2020,
     type: "W",
-    title: "2020_W Varshavskoye Highway 141",
+    title: "Varshavskoye Highway 141",
     slug: "2020_W_Varshavskoye Highway_141",
     cover: "assets/img/2020_W_Varshavskoye_Highway_141-cover.webp"
   },
     {
     year: 2022,
     type: "C",
-    title: "2022_C Devinska Kobyla",
+    title: "Devinska Kobyla",
     slug: "2022_C_Devinska_Kobyla",
     cover: "assets/img/2022_C_Devinska_Kobyla 6-cover.webp"
   },
